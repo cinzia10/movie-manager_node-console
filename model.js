37c492e61e7e67fc1e6a34cb41e729fa377e70d5
cinzia10 <103512084+cinzia10@users.noreply.github.com>
2022-05-17 21:13:51 +0200

    Add files via upload

--- model.js
+++ model.js
@@ -1,82 +1,23 @@
+class Movie{
 
-const prompt = require('prompt');
 
-class Movie {
-    constructor(title, genre, yop) {
-        this.title = title;
-        this.genre = genre;
-        this.yop = yop;
+    constructor(title, genre, yob) {
+      this.title = title;
+      this.genre = genre;
+      this.yob = yob;
+    }
+  
+    toString(){
+        const pubString = 'Titolo: ' + this.title + '\n' +
+                          'Genere: ' + this.genre + '\n' +
+                          'Anno di uscita: ' + this.yob;
+      return pubString;
     }
 }
 
-prompt.start();
-
-function startMenu() {
-    console.log('sono disponibili tre opzioni');
-    console.log('1) aggiungi un film');
-    console.log('2) lista film');
-    console.log('3) esci')
-
-    const schema = {
-        properties: {
-            selection: {
-                description: 'Seleziona una delle opzioni',
-            }
-        }
-    }
-    prompt.get(schema, startMenuManager);
+function movieFactory(obj) {  
+  return new Movie(obj.title, obj.genre, obj.yob)
 }
-
-
-function startMenuManager(err, result) {
-    if (result.selection === '1') {
-        console.log('scelta 1');;
-    } else if (result.selection === '2') {
-        console.log('scelta 2');;
-    } else if (result.selection === '3') {
-        console.log('Scelta 3')
-        process.exit();
-    } else {
-        console.log('selezione non disponibile');
-        startMenu();
-    }
-}
-
-
-function saveData(arrayToSave) {
-    const jsonArray = JSON.stringify(arrayToSave);
-
-    try {
-        fs.writeFileSync('./movie-data.json', jsonArray);
-    } catch (error) {
-        console.log('impossibile salvare il file')
-    }
-}
-
-function loadData() {
-    let jsonArray = '[]';
-
-    try {
-        jsonArray = fs.readFileSync('./movie-data.json', 'utf8');
-    } catch (error) {
-        jsonArray = '[]';
-    }
-
-    jsonArray = jsonArray.trim();
-    let array = [];
-    if (jsonArray) {
-        array = JSON.parse(jsonArray);
-    }
-
-    const pubArray = [];
-
-    for (const obj of array) {
-        const publication = model.publicationFactory(obj);
-        pubArray.push(publication)
-    }
-    return pubArray
-}
-
 
 exports.Movie = Movie;
-exports.startMenu = startMenu;
+exports.movieFactory = movieFactory;
--- script.js
+++ script.js
@@ -1,9 +1,119 @@
-
-
+const prompt = require('prompt');
 const model = require('./model.js');
+const fs = require('fs')
 
-const fs = require('fs');
+const movieArray = loadData();
 
-const movieArray = [];
 
-model.startMenu();
+console.log('Benvenuto in MOVIE MANAGER');
+
+startMenu();
+
+
+
+
+////////// MENU' PRINCIPALE
+function startMenu() {
+
+    console.log('Sono disponibili tre opzioni');
+    console.log('1)  Aggiungere film');
+    console.log('2)  Lista dei film');
+    console.log('3)  Esci');
+
+    prompt.start();
+
+const schema = {
+    properties: {
+      selection: {
+        description: 'Seleziona una delle opzioni',
+      }
+    }
+  };
+
+  prompt.get(schema, startMenuManager);
+
+}
+function startMenuManager(err, result) {
+      if (result.selection === '1') {
+        insertMovie();
+      } else if (result.selection === '2') {
+        printMovieList(movieArray);  
+        // printBooks(bookArray);
+      } else if (result.selection === '3') {
+        console.log('Grazie e a presto!')
+        process.exit();
+      } else {
+          console.log('Selezione non disponibile');
+          startMenu();
+      }
+}
+
+////////// AGGIUNGERE FILM
+function insertMovie() {
+    const schema = {
+        properties: {
+          title: {
+            description: 'Inserisci il titolo',
+          },
+          genre: {
+            description: 'Inserisci il genere',
+          },
+          yop: {
+            description: 'Inserisci l\'anno di uscita',
+          }
+         
+        }};
+    
+      prompt.get(schema, insertMovieManager);
+  };
+
+function insertMovieManager(err, result) {
+    const movie = new model.Movie(result.title, result.genre, result.yob);
+    movieArray.push(movie);
+    saveData(movieArray);
+    startMenu();
+}
+
+////////// PRINT LISTA DEI FILM
+function printMovieList(array) {
+  for (const movie of array) {
+      console.log(movie.toString())
+  }
+}
+
+
+////////// SALVARE LE INFORMAZIONI
+function saveData(arrayToSave) {
+    const jsonArray = JSON.stringify(arrayToSave);
+    try {
+        fs.writeFileSync('./data-file.json', jsonArray);
+    } catch (error) {
+        console.error('Impossibile salvare il file ')
+    }
+
+}
+
+////////// CARICARE LE INFORMAZIONI
+function loadData() {
+    let jsonArray
+
+    try {
+        jsonArray = fs.readFileSync('./data-file.json', 'utf8');
+    } catch (error) {
+        jsonArray = '[]'
+    }
+    jsonArray = jsonArray.trim();
+    let array = [];
+    if (jsonArray) {
+        array = JSON.parse(jsonArray);
+    }
+   
+    const movArray = [];
+
+    for (const obj of array) {
+        const movie = model.movieFactory(obj);
+        movArray.push(movie)
+    }
+    return movArray;
+
+}
